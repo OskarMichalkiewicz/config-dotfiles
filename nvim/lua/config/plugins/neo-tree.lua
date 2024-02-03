c81d2255bb10e2d159e04b6c4cc8cc58cdaf9d5b
Oskar Michalkiewicz <oskar.michalkiewicz@gmail.com>
2024-02-03 22:51:56 +0100

    migrate from packer to lazy, add a few plugins

---
+++ nvim/lua/config/plugins/neo-tree.lua
@@ -0,0 +1,105 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        {
+            's1n7ax/nvim-window-picker',
+            version = '2.*',
+            config = function()
+                require('window-picker').setup({
+                    filter_rules = {
+                        include_current_win = false,
+                        autoselect_one = true,
+                        -- filter using buffer options
+                        bo = {
+                            -- if the file type is one of following, the window will be ignored
+                            filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                            -- if the buffer type is one of following, the window will be ignored
+                            buftype = { 'terminal', "quickfix" },
+                        },
+                    },
+                })
+            end,
+        },
+    },
+    config = function()
+        vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+        vim.keymap.set('n', '<C-`>', '<Cmd>Neotree focus<CR>')
+        require("neo-tree").setup({
+            filesystem = {
+                follow_current_file = true,
+            },
+            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            enable_normal_mode_for_inputs = false, -- Enable normal mode for input dialogs.
+            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
+            sort_case_insensitive = false, -- used when sorting files and directories in the tree
+            sort_function = nil , -- use a custom function for sorting files and directories in the tree 
+            -- sort_function = function (a,b)
+            --       if a.type == b.type then
+            --           return a.path > b.path
+            --       else
+            --           return a.type > b.type
+            --       end
+            --   end , -- this sorts files and directories descendantly
+            --           default_component_configs = {
+            container = {
+                enable_character_fade = true
+            },
+            indent = {
+                indent_size = 2,
+                padding = 1, -- extra padding on left hand side
+                -- indent guides
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+                -- expander config, needed for nesting files
+                with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+            },
+            icon = {
+                folder_closed = "",
+                folder_open = "",
+                folder_empty = "󰜌",
+                -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+                -- then these will never be used.
+                default = "*",
+                highlight = "NeoTreeFileIcon"
+            },
+            modified = {
+                symbol = "[+]",
+                highlight = "NeoTreeModified",
+            },
+            name = {
+                trailing_slash = false,
+                use_git_status_colors = true,
+                highlight = "NeoTreeFileName",
+            },
+            git_status = {
+                symbols = {
+                    -- Change type
+                    added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                    modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                    deleted   = "✖",-- this can only be used in the git_status source
+                    renamed   = "󰁕",-- this can only be used in the git_status source
+                    -- Status type
+                    untracked = "",
+                    ignored   = "",
+                    unstaged  = "󰄱",
+                    staged    = "",
+                    conflict  = "",
+                }
+            },
+        })
+    end
+
+}
